7a78296410d6ed55a101aa628d8c3a4cb1c3de40
Joerg Knoche <joerg.knoche@hs.uni-hamburg.de>
2021-05-17 14:44:23 +0200

    check if menus folder exists, before trying to create it

--- daiquiri_navigation.php
+++ daiquiri_navigation.php
@@ -4,8 +4,9 @@ add_action('wp_update_nav_menu', 'daiquiri_update_nav_menu');
 
 function daiquiri_update_nav_menu() {
     // check if the wp-content/menus directory exists
-    mkdir(WP_CONTENT_DIR . '/menus/', 0755);
-
+    if (!is_dir(WP_CONTENT_DIR . '/menus/')) {
+        mkdir(WP_CONTENT_DIR . '/menus/', 0755);
+    }
     // loop over menus and save the html in the wp-content/menus directory
     foreach (get_terms( 'nav_menu') as $menu) {
         $html = wp_nav_menu(array(
